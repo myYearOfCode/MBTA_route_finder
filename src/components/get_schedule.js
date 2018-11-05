581bd72fdb5186681710e4ac24edd29982205085
Ross <Ross@myyearofcode.com>
2018-11-04 22:15:13 -0500

    init commit get_schedule.

---
+++ src/components/get_schedule.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export default class GetSchedule extends Component {
+	// make a function that receives the stop in props and runs the api call
+	// here is the porter api call https://api-v3.mbta.com/schedules?filter[stop]=70065
+	render() {
+		return <div>Get_Schedule</div>;
+	}
+}
