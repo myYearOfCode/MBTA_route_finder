3f3bfb1836d8035a8ee65ecfd103fcb30302af49
Ross <Ross@myyearofcode.com>
2018-11-05 22:20:39 -0500

    wiring in get all lines to main

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import GetSchedule from "./get_schedule";
+import SearchBar from "./search_bar";
+import AllLines from "./all_lines";
 
 export default class App extends Component {
-  render() {
-    return (
-      <div>React simple starter</div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<SearchBar />
+				<GetSchedule />
+				<AllLines />
+			</div>
+		);
+	}
 }
