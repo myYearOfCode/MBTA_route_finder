042ddb8b318422f47f56645b7b7aecb45b3660aa
Ross <Ross@myyearofcode.com>
2018-11-07 22:43:04 -0500

    add line selector to app.js

--- src/components/app.js
+++ src/components/app.js
@@ -3,11 +3,21 @@ import GetSchedule from "./get_schedule";
 import SearchBar from "./search_bar";
 import AllLines from "./all_lines";
 import PredictedSchedule from "./predicted_schedule";
+import LineSelector from "./line_selector";
+
+let selectLine = line => {
+	this.setState.line = line;
+};
 
 export default class App extends Component {
+	constructor(props) {
+		super(props);
+	}
+
 	render() {
 		return (
 			<div>
+				<LineSelector />
 				<PredictedSchedule />
 				<SearchBar />
 				<GetSchedule />
