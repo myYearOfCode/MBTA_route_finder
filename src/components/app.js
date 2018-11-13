9505b9eeb54db8f8c1cef8a8d809aed142d20e06
Ross <Ross@myyearofcode.com>
2018-11-12 22:01:50 -0500

    add all stops wiring. temporarily unwire most else

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import GetSchedule from "./get_schedule";
 import SearchBar from "./search_bar";
+import AllStops from "./all_stops";
 import AllLines from "./all_lines";
 import PredictedSchedule from "./predicted_schedule";
 import LineSelector from "./line_selector";
@@ -13,20 +14,28 @@ export default class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			line: "",
+			line: "Red",
 			stop: ""
 		};
 	}
 
 	render() {
+		let handleLineSelector = line => {
+			console.log("hello");
+			this.setState({ line });
+		};
+
 		return (
 			<div>
-				<LineSelector />
-				<PredictedSchedule />
-				<SearchBar />
-				<GetSchedule />
-				<AllLines />
+				{this.state.line}
+				<LineSelector onLineSelectionChange={handleLineSelector} />
+
+				<AllStops line={this.state.line} />
 			</div>
 		);
 	}
 }
+// <PredictedSchedule />
+// <SearchBar />
+// <GetSchedule />
+// <AllLines />
