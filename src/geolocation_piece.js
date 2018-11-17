d16e6a9af8358f673c1a13692aabc8024a7511eb
Ross <Ross@myyearofcode.com>
2018-11-16 22:07:13 -0500

    add planning comments

--- src/geolocation_piece.js
+++ src/geolocation_piece.js
@@ -17,3 +17,7 @@ function successFunction(position) {
 	var long = position.coords.longitude;
 	console.log("Your latitude is :" + lat + " and longitude is " + long);
 }
+
+//next up would be to iterate through the stops and calculate the closest few stops. I guess that would be the square root of ((lat-lat2)^2 + (lon-lon2)^2)
+
+//I wonder how long it will take to iterate over all 8563 stops...
