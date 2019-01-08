05fbebf8c1c90df1a330825bb4345406392bc92c
Ross <Ross@myyearofcode.com>
2019-01-07 23:10:19 -0500

    fix green line. add html title

--- src/components/line_selector.js
+++ src/components/line_selector.js
@@ -3,18 +3,19 @@ import { DropdownButton } from 'react-bootstrap';
 
 //TODO
 // +++import colors and display them in the div bkgs
-//set up the divs to be clickable
-//capture the urls for the vehicle schedules
+// should I combine all green lines into one line?
+// set up the divs to be clickable
+// capture the urls for the vehicle schedules
 // import all lines, filter by bus / train
 // 1 = subway, 3 = bus "filter%5Broute_type%5D=1"
 // https://api-v3.mbta.com/routes?filter%5Broute_type%5D=1
-//then show the line stops and directions
+// then show the line stops and directions
 // https://api-v3.mbta.com/stops?filter%5Broute%5D=Red
-//then show the predictions for that stop
-//ideally they are all stateless and in the main app
-//reacting to the previous input
-//have fun
-//learn shit
+// then show the predictions for that stop
+// ideally they are all stateless and in the main app
+// reacting to the previous input
+// have fun
+// learn shit
 
 export default class LineSelector extends Component {
 	constructor(props) {
@@ -34,7 +35,10 @@ export default class LineSelector extends Component {
 
 			>
 				<option value="Blue">Blue</option>
-				<option value="Green">Green</option>
+				<option value="Green-B">Green B</option>
+				<option value="Green-C">Green C</option>
+				<option value="Green-D">Green D</option>
+				<option value="Green-E">Green E</option>
 				<option value="Orange">Orange</option>
 				<option value="Red" defaultValue>Red</option>
 			</select>
